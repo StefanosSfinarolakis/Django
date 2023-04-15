cf182a2d338209c583b468b71460a5ae5c3ba8db
inf2021090 <inf2021090@ionio.gr>
2023-04-15 17:22:37 +0300

    split rest-backend st-frontend

--- streamlit_frontend/main.py
+++ streamlit_frontend/main.py
@@ -1,1 +1,17 @@
+import streamlit as st
 
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+
+st.write("# Welcome to ImageWeave! 👋")
+
+st.sidebar.success("Select a mode above.")
+
+st.markdown(
+    """
+
+    """
+)

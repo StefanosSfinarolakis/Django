1e63d35e699f7349002c0732cc97dca1f042d35e
inf2021090 <inf2021090@ionio.gr>
2023-04-19 14:04:51 +0300

    update settings

--- django_backend/ImageWeave/urls.py
+++ django_backend/ImageWeave/urls.py
@@ -27,3 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT) 
+#specifies url of media
